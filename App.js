69350ee081de0639703a3bd1104624dc382ecc08
Samuel Solomon <samuelsolomon452@gmail.com>
2024-03-22 11:57:32 +0300

    creating navigation icons

--- App.js
+++ App.js
@@ -9,10 +9,26 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 
+import Ionicons from "@expo/vector-icons/Ionicons";
 const Tab = createBottomTabNavigator();
+
+const Tab_Icon = {
+  Restaurant: "md-restaurant",
+  Map: "md-map",
+  Settings: "md-settings",
+}
 
 const Settings =()=> <Text>Settings</Text>;
 const Map =()=> <Text>Map</Text>;
+
+const CreateScreenOptions = ({route}) => {
+  const iconName = Tab_Icon[route.name];
+  return {
+    tabBarIcon:({size, color }) =>(
+      <Ionicons name={iconName} size={size} color={color} />
+    )
+  }
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -27,7 +43,12 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+        screenOptions={CreateScreenOptions}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "gray",
+        }}>
           <Tab.Screen name="Restaurant" component={RestaurantsScreen} />
           <Tab.Screen name="Map" component={Map} />
           <Tab.Screen name="Settings" component={Settings} />
